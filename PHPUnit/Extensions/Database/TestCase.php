1557bad5dd6afec0a7acada6e711396fdbcbe82d
Sebastian Marek <proofek@gmail.com>
2011-05-19 08:52:38 +0100

    Fix #38 - Add assertTableContains assertion

--- PHPUnit/Extensions/Database/TestCase.php
+++ PHPUnit/Extensions/Database/TestCase.php
@@ -262,4 +262,16 @@ abstract class PHPUnit_Extensions_Database_TestCase extends PHPUnit_Framework_Te
 
         self::assertThat($actual, $constraint, $message);
     }
+
+    /**
+     * Asserts that a given table contains a given row
+     *
+     * @param array $expectedRow Row expected to find
+     * @param PHPUnit_Extensions_Database_DataSet_ITable $table Table to look into
+     * @param string $message Optional message
+     */
+    public function assertTableContains(array $expectedRow, PHPUnit_Extensions_Database_DataSet_ITable $table, $message = '')
+    {
+        self::assertThat($table->assertContainsRow($expectedRow), self::isTrue(), $message);
+    }
 }
