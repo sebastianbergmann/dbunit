00dd9ec393ec4d50335d94c4c7ffd88514b51644
zamrzlaj <josef.zamrzla@lmc.eu>
2012-11-28 09:59:14 +0100

    Bug fix: Fatal error:  Cannot call abstract method PHPUnit_Extensions_Database_TestCase::getConnection()

--- PHPUnit/Extensions/Database/TestCase.php
+++ PHPUnit/Extensions/Database/TestCase.php
@@ -258,7 +258,7 @@ abstract class PHPUnit_Extensions_Database_TestCase extends PHPUnit_Framework_Te
     public function assertTableRowCount($tableName, $expected, $message = '')
     {
         $constraint = new PHPUnit_Extensions_Database_Constraint_TableRowCount($tableName, $expected);
-        $actual = self::getConnection()->getRowCount($tableName);
+        $actual = $this->getConnection()->getRowCount($tableName);
 
         self::assertThat($actual, $constraint, $message);
     }
