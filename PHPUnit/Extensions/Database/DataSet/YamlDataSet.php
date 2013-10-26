052d027ffac5bad2211a2a9b2da30a18e41e1a61
Yash Parghi <yash@etsy.com>
2013-10-26 19:10:47 -0400

    Move the new YAML class and interface into their own files.

--- PHPUnit/Extensions/Database/DataSet/YamlDataSet.php
+++ PHPUnit/Extensions/Database/DataSet/YamlDataSet.php
@@ -57,7 +57,6 @@
  */
 class PHPUnit_Extensions_Database_DataSet_YamlDataSet extends PHPUnit_Extensions_Database_DataSet_AbstractDataSet
 {
-
     /**
      * @var array
      */
@@ -171,41 +170,5 @@ class PHPUnit_Extensions_Database_DataSet_YamlDataSet extends PHPUnit_Extensions
               __METHOD__ . ' called with an unwritable file.'
             );
         }
-    }
-}
-
-/**
- * An interface for parsing YAML files.
- *
- * @package    DbUnit
- * @author     Yash Parghi <yash@yashparghi.com>
- * @copyright  2013 Sebastian Bergmann <sebastian@phpunit.de>
- * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
- * @link       http://www.phpunit.de/
- * @since      Class available since Release 1.3.1
- */
-interface PHPUnit_Extensions_Database_DataSet_IYamlParser {
-
-    /**
-     * @param string $yamlFile
-     * @return array parsed YAML
-     */
-    public function parseYaml($yamlFile);
-}
-
-/**
- * The default YAML parser, using Symfony/Yaml.
- *
- * @package    DbUnit
- * @author     Yash Parghi <yash@yashparghi.com>
- * @copyright  2013 Sebastian Bergmann <sebastian@phpunit.de>
- * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
- * @link       http://www.phpunit.de/
- * @since      Class available since Release 1.3.1
- */
-class PHPUnit_Extensions_Database_DataSet_SymfonyYamlParser implements PHPUnit_Extensions_Database_DataSet_IYamlParser {
-
-    public function parseYaml($yamlFile) {
-        return Symfony\Component\Yaml\Yaml::parse($yamlFile);
     }
 }
