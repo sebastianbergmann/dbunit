76adbf67d800f9e275e4bb70f1106d02f152c7a4
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-11-03 15:01:44 +0100

    Leftover from 3ccd22500a1cc0e5c05cdeff2f1fc721c3f2fbcb

--- build/travis-ci.sh
+++ build/travis-ci.sh
@@ -17,5 +17,5 @@ ${COMPOSER} install --no-interaction --prefer-source --dev
 
 mysql -u ${MYSQL_USER} -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 
-sed -i 's/<!--//g' build/travis-ci.xml
-sed -i 's/-->//g' build/travis-ci.xml
+sed -i 's/<!--//g' build/phpunit.xml
+sed -i 's/-->//g' build/phpunit.xml
