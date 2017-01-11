b180150222b6b8b8ffc0de11d5124a50f0eb5523
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-01-11 11:18:51 +0100

    Rename PHPUnit_Extensions_Database_DB_TableMetaData to PHPUnit\DbUnit\Database\Metadata\Table

--- src/Database/Metadata/Table.php
+++ src/Database/Metadata/Table.php
@@ -7,17 +7,20 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-use PHPUnit\DbUnit\Database\Metadata\IMetadata;
+
+namespace PHPUnit\DbUnit\Database\Metadata;
+
+use PHPUnit_Extensions_Database_DataSet_DefaultTableMetaData;
 
 /**
  * This class loads a table metadata object with database metadata.
  */
-class PHPUnit_Extensions_Database_DB_TableMetaData extends PHPUnit_Extensions_Database_DataSet_DefaultTableMetaData
+class Table extends PHPUnit_Extensions_Database_DataSet_DefaultTableMetaData
 {
     public function __construct($tableName, IMetadata $databaseMetaData)
     {
-        $this->tableName   = $tableName;
-        $this->columns     = $databaseMetaData->getTableColumns($tableName);
+        $this->tableName = $tableName;
+        $this->columns = $databaseMetaData->getTableColumns($tableName);
         $this->primaryKeys = $databaseMetaData->getTablePrimaryKeys($tableName);
     }
 }
